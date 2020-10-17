253da5984d533b4178b2899b142c4cb93e1f9fd8
mhorod <michalhorodecki2002@gmail.com>
2020-10-17 13:30:58 +0200

    Bytes and text encoding. Added more tests

--- RSA/rsa.py
+++ RSA/rsa.py
@@ -1,4 +1,5 @@
 import secrets
+import base64
 
 
 class Key:
@@ -10,7 +11,7 @@ class Key:
 def random_prime(bits=256):
     attempt = 0
     value = secrets.randbits(bits)
-    value |= (1 << (bits - 1)) + 1
+    value |= (1 << (bits - 1)) + 1  # Set up first and last bit
     while True:
         attempt += 1
         if is_prime(value):
@@ -32,8 +33,8 @@ def seems_prime(value):
     """Check whether given value seems prime using Rabin-Miller algorithm"""
     if value == 2: return True
     if value % 2 == 0: return False
-    lsb = find_lsb(value - 1)
 
+    lsb = find_lsb(value - 1)
     d = (value - 1) // (1 << lsb)
 
     # a is a witness or a liar for compositness of value
@@ -95,10 +96,10 @@ def egcd(a, b):
         return (g, y, x - (a // b) * y)
 
 
-def generate_keys():
+def generate_keys(bits=256):
     """Generate public and private RSA keys"""
-    p = random_prime(256)
-    q = random_prime(256)
+    p = random_prime(bits)
+    q = random_prime(bits - 1)  # So that p != q
     N = p * q
     totient = (p - 1) * (q - 1)
     while True:
@@ -111,38 +112,100 @@ def generate_keys():
     return Key(e, N), Key(d, N)
 
 
-def encode(value, key):
+def encrypt_integer(value, key):
     return fast_power(value, key.exponent, key.modulus)
 
-def encode_array(values, key):
-    return [encode(value, key) for value in values]
+
+def encrypt_array(values, key):
+    return [encrypt_integer(value, key) for value in values]
 
 
-def chunks(values, chunk_size):
-    for i in range(0, len(values), chunk_size):
-        yield values[i:i + chunk_size]
+def join_while_less(byte_data, value):
+    current_chunk = b''
+    for byte in byte_data:
+        byte = bytes([byte])
+        if int.from_bytes(current_chunk + byte, 'big') <= value:
+            current_chunk += byte
+        else:
+            yield current_chunk
+            current_chunk = byte
 
-def encode_bytes(data, key):
+    if current_chunk: yield current_chunk
+
+
+def encrypt_bytes(data, key):
+    chunk_size = key.modulus.bit_length() // 8
+    return encrypt_partitioned_bytes(partition_bytes(data, chunk_size), key)
+
+
+def decrypt_bytes(data, key):
+    return departiton_bytes(encrypt_partitioned_bytes(data, key))
+
+
+def encrypt_partitioned_bytes(data, key):
     result = b''
-    bytes_in_key = key.bit_length() // 8
-    for chunk in chunks(data, bytes_in_key):
-        chunk_value = int.from_bytes(chunk, 'big')
-        encoded_chunk_value = encode(chunk_value, key)
-        encoded_chunk = encoded_chunk_value.to_bytes(
-                (encoded_chunk_value.bit_length + 7) // 8, 'big')
-    
+    for chunk in paritioned_chunks(data):
+        chunk_value = int.from_bytes(chunk, 'little')
+        encrypted_chunk_value = encrypt_integer(chunk_value, key)
+        chunk_size = (encrypted_chunk_value.bit_length() + 7) // 8
+        encrypted_chunk = encrypted_chunk_value.to_bytes(chunk_size, 'little')
+        result += bytes([chunk_size]) + encrypted_chunk
+
     return result
 
-def encode_text(text, key):
-    text_bytes = bytes(text, "ascii")
-    return encode_bytes(text_bytes, key)
 
-def decode_to_text(data, keys):
-    pass
+def partition_bytes(data, chunk_size):
+    """Partition byte data into chunks (chunk_size, chunk_data)"""
+    result = b''
+    for i in range(0, len(data), chunk_size):
+        chunk = data[i:i + chunk_size]
+        result += bytes([len(chunk)]) + chunk
+    return result
 
-if __name__ == '__main__':
-    public, private = generate_keys()
-    value = b'1234'
-    v1 = encode_bytes(value, public)
-    v2 = encode_bytes(v1, private)
-    print(v1, v2)
+
+def departiton_bytes(data):
+    """Remove chunk_size information and convert partitioned data into original whole"""
+    result = b''
+    for chunk in paritioned_chunks(data):
+        result += chunk
+    return result
+
+
+def paritioned_chunks(paritioned_data):
+    """Return list of chunks from paritioned_data"""
+    chunks = []
+    i = 0
+    while i < len(paritioned_data):
+        chunk_size = paritioned_data[i]
+        chunk_data = paritioned_data[i + 1:i + chunk_size + 1]
+        chunks.append(chunk_data)
+        i += chunk_size + 1
+    return chunks
+
+
+def encrypt_text(text, key, encoding="ascii"):
+    text_bytes = encode_text_to_bytes(text, encoding)
+    b = encrypt_bytes(text_bytes, key)
+    return encode_bytes_to_text(b, encoding)
+
+
+def decrypt_text(text, key, encoding="ascii"):
+    text_bytes = decode_bytes_from_text(text, encoding)
+    b = decrypt_bytes(text_bytes, key)
+    return decode_text_from_bytes(b, encoding)
+
+
+def encode_text_to_bytes(text, encoding='ascii'):
+    return base64.b64encode(bytes(text, encoding))
+
+
+def decode_text_from_bytes(data, encoding='ascii'):
+    return str(base64.b64decode(data), encoding)
+
+
+def encode_bytes_to_text(data, encoding='ascii'):
+    return str(base64.b64encode(data), encoding)
+
+
+def decode_bytes_from_text(text, encoding='ascii'):
+    return base64.b64decode(bytes(text, encoding))
--- RSA/test.py
+++ RSA/test.py
@@ -1,10 +1,18 @@
+import random
+
 import pytest
 
 import rsa
 
 
 def test_fast_power():
-    assert rsa.fast_power(38, 107, 215) == 197
+    repeat_count = 20
+    for _ in range(repeat_count):
+        base = random.randint(1, 1000)
+        exponent = random.randint(1, 1000)
+        modulus = random.randint(1, 100000)
+        assert rsa.fast_power(base, exponent,
+                              modulus) == (base**exponent) % modulus
 
 
 def test_is_prime():
@@ -30,3 +38,26 @@ def test_egcd():
     v2 = 13
     g, x, y = rsa.egcd(v1, v2)
     assert g == 1 and (v1 * x + v2 * y) == g
+
+
+def test_encrypted_integer_can_be_decrypted():
+    repeat_count = 10
+    for _ in range(repeat_count):
+        public, private = rsa.generate_keys()
+        value = random.randint(1, public.modulus - 1)
+        assert rsa.encrypt_integer(rsa.encrypt_integer(value, public),
+                                   private) == value
+
+
+def test_encrypted_bytes_can_be_decrypted():
+    public, private = rsa.generate_keys()
+    data_size = 256
+    data = bytes([random.randint(0, 255) for _ in range(data_size)])
+    assert data == rsa.decrypt_bytes(rsa.encrypt_bytes(data, public), private)
+
+
+def test_encrypted_text_can_be_encrypted():
+    public, private = rsa.generate_keys()
+    text_length = 256
+    text = ''.join(chr(random.randint(32, 125)) for _ in range(text_length))
+    assert text == rsa.decrypt_text(rsa.encrypt_text(text, public), private)
